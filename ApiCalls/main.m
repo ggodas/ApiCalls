a9e7142def41555a85f9ea18c3fa49254db73528
George Krajan Godas <georgegodas@Georges-MBP.cloudsol.com.br>
2015-02-26 18:48:08 -0300

    Initial Commit

---
+++ ApiCalls/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ApiCalls
+//
+//  Created by George Krajan Godas on 2/26/15.
+//  Copyright (c) 2015 George Krajan Godas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
